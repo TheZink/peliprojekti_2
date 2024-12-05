4dbd217f97ac3b944508d1411a172f946ca2381a
TheZink <ilkka.sinkonen@gmail.com>
2024-12-05 09:13:23 +0200

    Tein testikansion

---
+++ Test/game_var.py
@@ -0,0 +1,21 @@
+# All game global variables
+
+player_name = ""
+
+player_id = 0
+
+game_airports = {}
+
+boxes_to_transport = 15
+
+home_airport = "EFHK"
+
+home_continent = "EU"
+
+airplane = 1
+
+boxes_delivered = 0 
+
+boxes_in_plane = 0
+
+#plane_cap = game_sql.airplane_info(airplane)[3]
