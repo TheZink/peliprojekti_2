05fd2d08e06dd9c1977a039132154b5121605958
TheZink <ilkka.sinkonen@gmail.com>
2024-12-08 14:44:36 +0200

    Backendissä muutoksia

--- Test/backend/main.py
+++ Test/backend/main.py
@@ -2,190 +2,212 @@
 from geopy.distance import geodesic
 import mysql.connector, random
 from flask import Flask, request
+from database import Database
 import json
 
 app = Flask(__name__)
+db=Database()
 
-class Database:
+# -------------------------------------------------DATABASE----------------------------------------------------------#
 
-    def __init__(self, player_id, player_name, boxes_to_transport, boxes_delivered,
-                boxes_in_plane, plane_id, game_airports, home_airport, home_continent, ident):
-        
-        self.player_id = player_id
-        self.player_name = player_name
-        self.boxes_to_transport = boxes_to_transport
-        self.boxes_delivered = boxes_delivered
-        self.boxes_in_plane = boxes_in_plane
-        self.plane_id = plane_id
-        self.game_airports = game_airports
-        self.home_airport = home_airport
-        self.home_continent = home_continent
-        self.ident = ident
-        
-        self.yhteys = mysql.connector.connect(
-            host="localhost",
-            port=3306,
-            database="rahtipeli",
-            user="pilotti",
-            password="pilotti12345",
-            autocommit=True,
-            collation='utf8mb4_unicode_ci'
-            )
+# Function to create a new player in the database. Function return the player ID in json.
+# Require two arguments <player_name>, <home_airport>.
 
-    @app.route('/create_users')    
-    def create_users(self):
-        args = request.args
-        # tähän kohtaan ne argumentit mitä kysellään
-        # esim
-        # url on muotoa: /create_users?player_name=Kalle& jne....
-        name = args.get("player_name")
-        sql = f"INSERT INTO PLAYER (name,ap_ident,distance,used_time,cons_gas,money,score) VALUES ('{name}','{self.home_airport}',0,0,0,0,0);"
-        kursori = self.yhteys.cursor()
-        kursori.execute(sql)
-        user_id = kursori.lastrowid
+# Function works
 
-        return user_id
+@app.route('/create_users/')    
+def create_users():
+    args = request.args
+    name = args.get("player_name")
+    home_airport = args.get("home_airport")
+
+    try:
+        sql = f"INSERT INTO PLAYER (name,ap_ident,distance,used_time,cons_gas,money,score) VALUES ('{name}','{home_airport}',0,0,0,0,0);"
+        cursor = db.get_conn().cursor()
+        cursor.execute(sql)
+        user_id = cursor.lastrowid
+
+        return {"user_id": user_id}, 200
+
+    except Exception as e:
+        return {"error": str(e)}, 400
 
 
-    # Function select randomly airports from designed continent and add them to dictionary.
-    # Its also select random values of boxes for each airports.
+# -------------------------------------------------------------------------------------------------------------------#
 
-    @app.route('/create_game')
-    def create_game(self):
-        sql = f"SELECT ident FROM airport WHERE continent = '{self.home_continent}'"
-        kursori = self.yhteys.cursor()
-        kursori.execute(sql)
-        tulos = kursori.fetchall()
+# Function select randomly (15) airports from designed continent and add them to dictionary.
+# Its also select random values of boxes for each airports (1-15).
+# Require one arguments <continent>
+
+# Function works 
+
+@app.route('/create_game/')
+def create_game():
+    args = request.args
+    continent = args.get("continent")
+
+    try:
+        sql = f"SELECT ident FROM airport WHERE continent = '{continent}'"
+        cursor = db.get_conn().cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        game_airports = {}
 
         for time in range(15):
-            choice_airport = random.choice(tulos)
-            if choice_airport not in self.game_airports:
-                # add new random airport to dictionary and add random 1-15 boxes to airport
-                self.game_airports[choice_airport[0]] = random.randint(1,15) #Generate random numbers for airport.
+            choice_airport = random.choice(result)
+            if choice_airport not in game_airports:
+                game_airports[choice_airport[0]] = random.randint(1,15)
+    
+    except Exception as e:
+        return {"error": str(e)}, 400
+    
+    return game_airports
 
-        return json.dumps(self.game_airports)
+# -------------------------------------------------------------------------------------------------------------------#
 
+# Fuction to get information of any given airport. Fuction return airport details (Name, city and country)
+# Return "ap name", "ap municipality", "ap country name" in tuple
+# Require one arguments <ident>
 
-    # Fuction to get information of any given airport. Fuction return airport details (Name, city and country)
-    @app.route('/get_information')
-    def get_information(self):
-        args = request.args
-        ident = args.get("ident")
-        sql = f"SELECT airport.name, airport.municipality, country.name FROM airport, country WHERE airport.ident = '{ident}' and airport.iso_country = country.iso_country;"
-        kursori = self.yhteys.cursor()
-        kursori.execute(sql)
-        tulos = kursori.fetchall()
+@app.route('/get_information/')
+def get_information():
+    args = request.args
+    ident = args.get("ident")
 
-        for info in tulos:
-            # return ap name, ap municipality, ap country name
-            return(info[0],info[1],info[2])
+    sql = f"SELECT airport.name, airport.municipality, country.name FROM airport, country WHERE airport.ident = '{ident}' and airport.iso_country = country.iso_country;"
+    kursori = db.get_conn.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
 
+    for info in tulos:
+        return(info[0],info[1],info[2]) 
+        
+# -------------------------------------------------------------------------------------------------------------------#
+# Function to get coordinates of any given airport identified by ident
+# Function return latitude, longitude coordinates in tuple
+# Require one arguments <ident>
 
-    # Function to get coordinates of any given airport identified by ident
-    @app.route('/get_coordinates')
-    def get_coordinates(self):
-        sql = f"SELECT airport.latitude_deg, airport.longitude_deg FROM airport WHERE airport.ident = '{self.ident}';"
-        kursori = self.yhteys.cursor()
-        kursori.execute(sql)
-        tulos = kursori.fetchall()
+@app.route('/get_coordinates/<ident>')
+def get_coordinates():
+    args = request.args
+    ident = args.get("ident")
 
-        for kordinaatti in tulos:
-            # return latitude, longitude coordinates
-            place = kordinaatti[0], kordinaatti[1]
-            return place # return coordinates
+    sql = f"SELECT airport.latitude_deg, airport.longitude_deg FROM airport WHERE airport.ident = '{ident}';"
+    kursori = db.get_conn.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
 
+    for kordinaatti in tulos:
+        return kordinaatti[0], kordinaatti[1]
 
-    # Function to get info from airplane name, model, fuel consume, capasity
-    @app.route('/airplane_info')
-    def airplane_info(self):
-        sql = f"SELECT airplane.plane_id, airplane.name, airplane.consume, airplane.speed, airplane.capasity FROM airplane WHERE airplane.plane_id = '{self.plane_id}';"
-        kursori = self.yhteys.cursor()
-        kursori.execute(sql)
-        tulos = kursori.fetchall()
+# -------------------------------------------------------------------------------------------------------------------#
+# Function to get info from airplane name, model, fuel consume, capasity
+# Function return "ap name", "ap fuel ratio litres per 100km per passenger", "ap speed in m/s" and "ap max box capasity"
+# Require one arguments <plane_id>
 
-        for content in tulos:
-            # return: ap name, ap fuel ratio litres per 100km per passenger, ap speed in m/s and ap max box capasity
-            return (content[1], content[2], content[3], content[4])
+@app.route('/airplane_info/<plane_id>')
+def airplane_info():
+    args = request.args
+    plane_id = args.get("plane_id")
 
+    sql = f"SELECT airplane.plane_id, airplane.name, airplane.consume, airplane.speed, airplane.capasity FROM airplane WHERE airplane.plane_id = '{plane_id}';"
+    kursori = db.get_conn.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
 
-    # Function to update player values
-    @app.route('/update_game')
-    def update_game(self):
-        # distance in km, used_time in hours, cons_gas in litres
-        sql = f"UPDATE player SET distance = distance+{Math.distance}, used_time = used_time+{Math.time_spent}, cons_gas = cons_gas+{Math.fuel_consumed} WHERE name = '{self.player_name}' and player_id = {self.player_id};"
-        kursori = self.yhteys.cursor()
-        kursori.execute(sql)
+    for content in tulos:
+        return (content[1], content[2], content[3], content[4])
 
+# -------------------------------------------------------------------------------------------------------------------#
+# Function to update player values. Values distance in km, used_time in hours, cons_gas in litres
+# Require five arguments <player_name>, <player_id>, <distance>, <time_spent>, <fuel_consumed>
 
-    # Function to get players info and
-    # return traveled distance, used time, used fuel, money and score
-    @app.route('/close_game')
-    def close_game(self,player_name: str):
-        sql = f"SELECT * FROM player WHERE name = '{player_name}' and player_id = {self.player_id}"
-        kursori = self.yhteys.cursor()
-        kursori.execute(sql)
-        tulos = kursori.fetchall()
+@app.route('/update_game')
+def update_game(self):
+    args = request.args
+    player_name = args.get("player_name")
+    player_id = args.get("player_id")
+    distance = args.get("distance")
+    time_spent = args.get("time_spent")
+    fuel_consumed = args.get("fuel_consumed")
 
-        for player in tulos:
-            # return: distance, used_time, cons_gas, money, score
-            return(player[3],player[4],player[5],player[6],player[7])
+    sql = f"UPDATE player SET distance = distance+{distance}, used_time = used_time+{time_spent}, cons_gas = cons_gas+{fuel_consumed} WHERE name = '{player_name}' and player_id = {player_id};"
+    kursori = db.get_conn.cursor()
+    kursori.execute(sql)
 
+# -------------------------------------------------------------------------------------------------------------------#
+# Function to get players info and
+# Return traveled distance, used time, used fuel, money and score (distance, used_time, cons_gas, money, score)
+# Require two arguments <player_name>, <player_id>
 
-class Math:
+@app.route('/close_game')
+def close_game():
+    args = request.args
+    player_name = args.get("player_name")
+    player_id = args.get("player_id")
 
-    def __init__(self, longitude1, latitude1,latitude2, longitude2, distance, fuel_burn_rate):  
-        self.latitude1 = latitude1
-        self.longitude1 = longitude1
-        self.latitude2 = latitude2
-        self.longitude2 = longitude2
-        self.distance = distance
-        self.fuel_burn_rate = fuel_burn_rate
+    sql = f"SELECT * FROM player WHERE name = '{player_name}' and player_id = {player_id}"
+    kursori = db.get_conn.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
 
-    # This function calculates the distance between any 2 airport locations given
-    # distance return in km
+    for player in tulos:
+        return(player[3],player[4],player[5],player[6],player[7])
 
-    @app.route('/distance_calculate')
-    def distance_calculate(self):
+# -------------------------------------------------MATH--------------------------------------------------------------#
 
-        self.location1 = (self.latitude1, self.longitude1)
-        self.location2 = (self.latitude2, self.longitude2)
+# This function calculates the distance between any 2 airport locations given and return distance in km
+# Require four arguments <latitude1>, <longitude1>, <latitude2>, <longitude2>
 
-        self.distance = geodesic(self.location1, self.location2).km
+@app.route('/distance_calculate')
+def distance_calculate(self):
+    args = request.args
+    latitude1 = args.get("latitude1")
+    longitude1 = args.get("longitude1")
+    latitude2 = args.get("latitude2")
+    longitude2 = args.get("longitude2")
 
-        # return distance in kilometers
-        # return distance
+    location1 = (latitude1, longitude1)
+    location2 = (latitude2, longitude2)
 
+    distance = geodesic(location1, location2).km
 
-    # This function calculates how much fuel airplane has used
-    # fuel_burn_rate is in database in airplane table
+    return distance
 
-    @app.route('/calculeta_fuel')
-    def calculate_fuel(self):
+# -------------------------------------------------------------------------------------------------------------------#
 
-        # fuel_burn_rate in database in litres per 100km per passenger
-        # calculate litres used
+# This function calculates how much fuel airplane has used. Function return used fuel in litres
+# Fuel_burn_rate is in database in airplane table and value is litres per 100km per passenger
+# Require two arguments <distance> and <fuel_burn_rate>
 
-        self.fuel_consumed = self.distance * (self.fuel_burn_rate / 100)
-        # return used fuel in litres
-        # return fuel_consumed
+@app.route('/calculeta_fuel')
+def calculate_fuel(self):
+    args = request.args
+    distance = args.get("distance")
+    fuel_burn_rate = args.get("fuel_burn_rate")
 
+    fuel_consumed = distance * (fuel_burn_rate / 100)
+    return fuel_consumed
 
-    # Function to calculate time spent while flying
-    # distance is in km
-    # speed in database is in metres per second
+# -------------------------------------------------------------------------------------------------------------------#
 
-    @app.route('/calculeta_time_spent')
-    def calculate_time_spent(self, speed):
-        self.speed = speed
-        self.time_spent = self.distance / (speed * 3.6)
+# Function to calculate time spent while flying. Distance is in km and speed in database is in metres per second. Return time spend in hours.
+# Require two arguments <speed> and <distance>
 
-        # return time_spend in hours
+@app.route('/calculeta_time_spent')
+def calculate_time_spent(speed):
+    args = request.args
+    speed = args.get("speed")
+    distance = args.get("distance")
+    
+    time_spent = distance / (speed * 3.6)
 
-        # return time_spent
+    return time_spent
 
-    # Function to calculate score for player
-    # def calculate_score(flight_time, fuel_used, money ):
+# Function to calculate score for player
+# def calculate_score(flight_time, fuel_used, money ):
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+    app.run(use_reloader=True, host='127.0.0.1', port=3000, debug=True)
