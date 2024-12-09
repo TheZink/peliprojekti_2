c6404fa2b54ec5688982108fb26aff558226c7c3
TheZink <ilkka.sinkonen@gmail.com>
2024-12-09 18:05:49 +0200

    Pitkä päivä

--- Test/backend/main.py
+++ Test/backend/main.py
@@ -1,22 +1,25 @@
 # This file contains all functions to and from our database
 from geopy.distance import geodesic
 import mysql.connector, random
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from database import Database
 import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 db=Database()
 
 # -------------------------------------------------DATABASE----------------------------------------------------------#
 
 # Function to create a new player in the database. Function return the player ID in json.
-# Require two arguments <player_name>, <home_airport>.
+# Require one arguments <player_name>
 
-# Function works
+# URL: /localhost/create_user?player_name=<name>
 
-@app.route('/create_users/')    
-def create_users():
+@app.route('/create_user/')    
+def create_user():
     args = request.args
     name = args.get("player_name")
     #home_airport = args.get("home_airport")
@@ -27,10 +30,10 @@ def create_users():
         cursor.execute(sql)
         user_id = cursor.lastrowid
 
-        return {"user_id": user_id}, 200
+        return jsonify({"user_id": user_id}), 200
 
     except Exception as e:
-        return {"error": str(e)}, 400
+        return jsonify({"error": str(e)}), 400
 
 
 # -------------------------------------------------------------------------------------------------------------------#
@@ -39,10 +42,10 @@ def create_users():
 # Its also select random values of boxes for each airports (1-15).
 # Require one arguments <continent>
 
-# Function works 
+# URL: /localhost/get_ap_idents?continent=<continent>
 
-@app.route('/create_game/')
-def create_game():
+@app.route('/get_ap_idents/')
+def get_ap_idents():
     args = request.args
     continent = args.get("continent")
 
@@ -57,74 +60,96 @@ def create_game():
         for time in range(15):
             choice_airport = random.choice(result)
             if choice_airport not in game_airports:
-                game_airports[choice_airport[0]] = random.randint(1,15)
+                game_airports[choice_airport[0]] = {'box': random.randint(1,15)}
+                # {EFHK: {BOX:15, jne.}, AAHK:{BOX:3, jne}}
+        
+        return jsonify(game_airports), 200
     
     except Exception as e:
-        return {"error": str(e)}, 400
+        return jsonify({"error": str(e)}), 400
     
-    return game_airports
-
+    
 # -------------------------------------------------------------------------------------------------------------------#
 
 # Fuction to get information of any given airport. Fuction return airport details (Name, city and country)
-# Return "ap name", "ap municipality", "ap country name" in tuple
+# Return "ap name", "ap municipality", "ap country name"
 # Require one arguments <ident>
 
-@app.route('/get_information/')
-def get_information():
+# URL: /localhost/get_ap_info?ident=<ident>
+
+@app.route('/get_ap_info/')
+def get_ap_info():
     args = request.args
     ident = args.get("ident")
 
-    sql = f"SELECT airport.name, airport.municipality, country.name FROM airport, country WHERE airport.ident = '{ident}' and airport.iso_country = country.iso_country;"
-    kursori = db.get_conn.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
+    try:
+        sql = f"SELECT airport.name, airport.municipality, country.name FROM airport, country WHERE airport.ident = '{ident}' and airport.iso_country = country.iso_country;"
+        cursor = db.get_conn().cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
 
-    for info in tulos:
-        return(info[0],info[1],info[2]) 
+        for info in result:
+            return jsonify({'name': info[0],'city':info[1],'country':info[2]}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
         
 # -------------------------------------------------------------------------------------------------------------------#
 # Function to get coordinates of any given airport identified by ident
 # Function return latitude, longitude coordinates in tuple
 # Require one arguments <ident>
 
-@app.route('/get_coordinates/<ident>')
-def get_coordinates():
+# URL: /localhost/get_ap_coordinates?ident=<ident>
+
+@app.route('/get_ap_coordinates/')
+def get_ap_coordinates():
     args = request.args
     ident = args.get("ident")
 
-    sql = f"SELECT airport.latitude_deg, airport.longitude_deg FROM airport WHERE airport.ident = '{ident}';"
-    kursori = db.get_conn.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
+    try:
+        sql = f"SELECT airport.latitude_deg, airport.longitude_deg FROM airport WHERE airport.ident = '{ident}';"
+        cursor = db.get_conn().cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
 
-    for kordinaatti in tulos:
-        return kordinaatti[0], kordinaatti[1]
+        for coordinates in result:
+            return jsonify({'lat':coordinates[0], 'long':coordinates[1]}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 # -------------------------------------------------------------------------------------------------------------------#
 # Function to get info from airplane name, model, fuel consume, capasity
 # Function return "ap name", "ap fuel ratio litres per 100km per passenger", "ap speed in m/s" and "ap max box capasity"
 # Require one arguments <plane_id>
 
-@app.route('/airplane_info/<plane_id>')
-def airplane_info():
+# URL: /localhost/get_airplane_info?plane_id=<plane_id>
+
+@app.route('/get_airplane_info/')
+def get_airplane_info():
     args = request.args
     plane_id = args.get("plane_id")
 
-    sql = f"SELECT airplane.plane_id, airplane.name, airplane.consume, airplane.speed, airplane.capasity FROM airplane WHERE airplane.plane_id = '{plane_id}';"
-    kursori = db.get_conn.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
+    try:
+        sql = f"SELECT airplane.plane_id, airplane.name, airplane.consume, airplane.speed, airplane.capasity FROM airplane WHERE airplane.plane_id = '{plane_id}';"
+        cursor = db.get_conn().cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
 
-    for content in tulos:
-        return (content[1], content[2], content[3], content[4])
+        for content in result:
+            return jsonify({'name':content[1], 'fuel':content[2], 'speed':content[3], 'capasity':content[4]}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400, 400
 
 # -------------------------------------------------------------------------------------------------------------------#
 # Function to update player values. Values distance in km, used_time in hours, cons_gas in litres
 # Require five arguments <player_name>, <player_id>, <distance>, <time_spent>, <fuel_consumed>
 
-@app.route('/update_game')
-def update_game(self):
+# URL: /localhost/update_player?player_name=<player_name>&player_id=<player_id>&distance=<distance>&time_spent=<time_spent>&fuel_consumed=<fuel_consumed>
+
+@app.route('/update_player/')
+def update_player():
     args = request.args
     player_name = args.get("player_name")
     player_id = args.get("player_id")
@@ -132,48 +157,68 @@ def update_game(self):
     time_spent = args.get("time_spent")
     fuel_consumed = args.get("fuel_consumed")
 
-    sql = f"UPDATE player SET distance = distance+{distance}, used_time = used_time+{time_spent}, cons_gas = cons_gas+{fuel_consumed} WHERE name = '{player_name}' and player_id = {player_id};"
-    kursori = db.get_conn.cursor()
-    kursori.execute(sql)
+
+    try:
+        sql = f"UPDATE player SET distance = distance + {distance}, used_time = used_time + {time_spent}, cons_gas = cons_gas + {fuel_consumed} WHERE name = '{player_name}' AND player_id = {player_id};"
+        kursori = db.get_conn().cursor()
+        kursori.execute(sql)
+
+        return jsonify({'status': 'true'}),200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 # -------------------------------------------------------------------------------------------------------------------#
 # Function to get players info and
 # Return traveled distance, used time, used fuel, money and score (distance, used_time, cons_gas, money, score)
 # Require two arguments <player_name>, <player_id>
 
-@app.route('/close_game')
-def close_game():
+# URL: /localhost/get_player_info?player_name=<player_name>&player_id=<player_id>
+
+@app.route('/get_player_info/')
+def get_player_info():
     args = request.args
     player_name = args.get("player_name")
     player_id = args.get("player_id")
 
-    sql = f"SELECT * FROM player WHERE name = '{player_name}' and player_id = {player_id}"
-    kursori = db.get_conn.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
+    try:
+        sql = f"SELECT * FROM player WHERE name = '{player_name}' and player_id = {player_id}"
+        cursor = db.get_conn().cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
 
-    for player in tulos:
-        return(player[3],player[4],player[5],player[6],player[7])
+        for player in result:
+            return jsonify({'distance':player[3],'used_time': player[4],'cons_gas': player[5],'money': player[6],'score': player[7]}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
 
 # -------------------------------------------------MATH--------------------------------------------------------------#
 
 # This function calculates the distance between any 2 airport locations given and return distance in km
 # Require four arguments <latitude1>, <longitude1>, <latitude2>, <longitude2>
 
-@app.route('/distance_calculate')
-def distance_calculate(self):
+# URL: /localhost/distance_calculate?lati1=<lati1>&long1=<long1>&lati2=<lati2>&long2=<long2>
+
+@app.route('/distance_calculate/')
+def distance_calculate():
     args = request.args
-    latitude1 = args.get("latitude1")
-    longitude1 = args.get("longitude1")
-    latitude2 = args.get("latitude2")
-    longitude2 = args.get("longitude2")
+    latitude1 = args.get("lati1")
+    longitude1 = args.get("long1")
+    latitude2 = args.get("lati2")
+    longitude2 = args.get("long2")
 
-    location1 = (latitude1, longitude1)
-    location2 = (latitude2, longitude2)
+    try:
+        location1 = (latitude1, longitude1)
+        location2 = (latitude2, longitude2)
 
-    distance = geodesic(location1, location2).km
+        distance = geodesic(location1, location2).km
 
-    return distance
+        return jsonify({'distance': distance}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 # -------------------------------------------------------------------------------------------------------------------#
 
@@ -181,29 +226,40 @@ def distance_calculate(self):
 # Fuel_burn_rate is in database in airplane table and value is litres per 100km per passenger
 # Require two arguments <distance> and <fuel_burn_rate>
 
-@app.route('/calculeta_fuel')
-def calculate_fuel(self):
+# URL: /localhost/calculate_fuel?distance=<distance>&fuel_burn_rate=<fuel_burn_rate>
+
+@app.route('/calculate_fuel')
+def calculate_fuel():
     args = request.args
     distance = args.get("distance")
     fuel_burn_rate = args.get("fuel_burn_rate")
 
-    fuel_consumed = distance * (fuel_burn_rate / 100)
-    return fuel_consumed
+    try:
+        fuel_consumed = int(distance) * (int(fuel_burn_rate) / 100)
+        return jsonify({'fuel': fuel_consumed}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 # -------------------------------------------------------------------------------------------------------------------#
 
 # Function to calculate time spent while flying. Distance is in km and speed in database is in metres per second. Return time spend in hours.
 # Require two arguments <speed> and <distance>
 
-@app.route('/calculeta_time_spent')
-def calculate_time_spent(speed):
+# URL: /localhost/calculate_time_spent?speed=<speed>&distance=<distance>
+
+@app.route('/calculate_time_spent')
+def calculate_time_spent():
     args = request.args
     speed = args.get("speed")
     distance = args.get("distance")
     
-    time_spent = distance / (speed * 3.6)
-
-    return time_spent
+    try:
+        time_spent = float(distance) / (float(speed) * 3.6)
+        return jsonify({'time_spent': time_spent}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 
 if __name__ == '__main__':
--- Test/js/main.js
+++ Test/js/main.js
@@ -1,65 +1,329 @@
 'use strict';
-/* 1. show map using Leaflet library. (L comes from the Leaflet library) */
 
-const map = L.map('map', {tap: false});
+// Game flow
+//
+//
+//
+// Peli alkaa pyytämällä pelaajalta nimen
+// ja sitten kutsumalla pelin aloittavia funktioita
+// ensin liitetään pelaajan nimeen pelaajalle annettava pelaajaid
+// joka syntyy tietokannan rivin järjestysnumerosta
+// sitten haetaan kaikki tarvittavat tiedot taustalta fronttiin
+// ja peli voi alkaa
+// pelaaja voi valita kartalta mille lentokentälle menee
+// laatikot siirtyvät kyytiin
+// ne pitää palauttaa kotiin
+// ja sitten hakea lisää
+// kunnes tarvittava määrä viety kotiin
+// pelin aikana päivitetään backendiä
+// ja kutsutaan päivitettyjä tietoja backendistä takas fronttiin
+//
+// pelaaja liikkuu kartalla klikkaamalla kartan markkereiden popup
+// puhekuplissa olevia "lennä tänne" nappeja
+//
+// lähdetään liikkeelle kotikentältä (Hki-Vantaa)
+// aina kun mennään toiselle kentälle niin
+//  otetaan laatikot kyytiin
+//  ja päivitetään lennetty matka, kulunut aika, syntynyt co2 tietokantaan
+// aina kun käydään kotona
+//  jätetään laatikot sinne
+//  ja päivitetään lennetty matka, kulunut aika, syntynyt co2 tietokantaan
+//  ja tarkastetaan onko laatikoita jo riittävästi
+//  jos riittävästi, game over ja tilastot näkyviin.
+
+
+
+//
+//
+// Map setup
+//
+//
+// Leaflet kirjasto ja Google kartta.
+/* 1. show map using Leaflet library. (L comes from the Leaflet library) */
+//
+// link L.map to constant
+const map = L.map('map', { tap: false });
+//
+// setup tileLayer from Google and add it to Map
 L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
   maxZoom: 20,
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
-map.setView([60, 24], 7);
+//
+// set startup view coordinates of the Map view
+map.setView([60, 24], 4);
 
-L.market([60,24]).addTo(map)
-.bindPopup('you are here!!!')
-.openPopup();
- 
 
+//
+//
 // global variables
-const apiUrl = 'http://127.0.0.1:3060'//start address to python
-const startLocation = ''; //from where we want to start we will put ident
-// icons
+//
+// address to backend
+const apiUrl = 'http://127.0.0.1:3000/';
+// from where we want to start we will put ident
+const startLocation = 'EFHK';
+const homeLocation = 'EFHK';
+const homeContinent = 'EU';
+const planeId = 1;
+let AirportsIdents = {};
+let userId = '';
+// create airport markers constant to map
+const airportMarkers = L.featureGroup().addTo(map);
+// OpenWeatherMap API key
+const APIkey = '7bfbeb2ebaec8cdb59103f744a3e8c1f';
 
+
+
+//
+//
+// icons definitions
+const blueIcon = L.divIcon({ className: 'blue-icon' });
+const greenIcon = L.divIcon({ className: 'green-icon' });
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// TÄSTÄ PELAAMINEN ALKAA
+//
+//
 // form for player name
-document.querySelector('#player-form').addEventListener('submit', function(evt){
+// aloitetaan peli pyytämällä pelaajalta pelaajan nimi
+// ja odottamalla että pelaaja klikkaa game start nappia
+document.querySelector('#player-form').addEventListener('submit', function (evt) {
   evt.preventDefault();
+  //
+  // save player name from form into constant
   const playerName = document.querySelector('#player-input').value;
-  //playerName variable will connect to backend player name
+  //
+  // hide player name form div
   document.querySelector('#start-form').classList.add('hide');
+  //
+  //
+  // call gamePlay function to start the game
+  gamePlay(playerName);
+  // GetAirports(homeContinent);
+  //
 })
 
+
+
+// Do we need this?
+//
 // function to fetch data from API
-async function getData(){
+async function getData(apiUrl) {
   const response = await fetch('apiUrl');
   if (!response.ok) throw new Error('Invalid server input!');
   const data = await response.json();
-  return data
+  return data;
 
 }
 
-// function to update game status
+//
+//
+// function to update game status to UI
+function updateStatus(status) {
 
-// function to show weather at selected airport
+  document.querySelector('#consumed').innerHTML = `${airport.co2_consumed}`;
+  document.querySelector('#budget').innerHTML = `${airport.co2_budget}`;
+  document.querySelector('#speed').innerHTML = `${airport.speed}`;
+  // document.querySelector('#laatiko-maara').innerHTML = status.
 
-// function to check if any goals have been reached
 
-// function to update goal data and goal table in UI
+}
 
-// function to check if game is over
+//
+//
+// function to show weather at selected airport in UI
+function showWeather(airport) {
+  document.querySelector('#airport-name').innerHTML = `Weather at ${airport.name}`;
+  document.querySelector('#weather-icon').src = airport.weather.icon;
+  document.querySelector('#airport-condition').innerHTML = airport.weather.description;
+  document.querySelector('#irport-temp').innerHTML = `${airport.weather.temp}`;
 
-// function to set up game
-// this is the main function that creates the game and calls the other functions
-//because we will to load data so is async function //this function is for loadind 
-//data and we could reuse that in another project too
-async function gameSetup() {
-  try{
-    const gameData = await getData();//????without parametr
-    //console.log(gameData);
-  }catch (error) {
+}
+
+
+// function to check if any goals have been reached?
+
+// function to update goal data and goal table in UI?
+
+// function to check if game is over?
+
+
+
+//
+//
+//
+// HERE WILL BE THE GAMEPLAY ENGINE FUNCTIONS
+//
+//
+//
+// To play
+// we need back player id, airplane info, home airport name and location, 15 airports idents, and their names and their locations
+//
+//
+
+
+async function gamePlay(playerName) {
+  // This create user by sending player name to backend, return with userid
+  try {
+    const responseName = await fetch(`${apiUrl}create_user?player_name=${playerName}`);
+    const NameData = await responseName.json();
+    userId = NameData.user_id;
+    console.log(userId);
+  } catch (error) {
     console.log(error);
+  }
+
+  // This create randomly 15 airports, return with each ident and each with random number of boxes as object literal?
+  try {
+    const responseGetAirports = await fetch(`${apiUrl}get_ap_idents?continent=${homeContinent}`);
+    AirportsIdents = await responseGetAirports.json();
+    // console.log(AirportsIdents);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // This get airplane info, return with name, fuel cons, airspeed, capasity
+  try {
+    const responseGetPlaneInfo = await fetch(`${apiUrl}get_airplane_info?plane_id=${planeId}`);
+    const AirPlaneInfoData = await responseGetPlaneInfo.json();
+    console.log(AirPlaneInfoData);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // This get airport info per Airport to put into UI
+  // /get_ap_info?ident=<ident>
+  // get AirPort info by ident: ap name, ap muni, ap country
+  // return info in JSON format
+
+  try {
+    for (let ident in AirportsIdents) {
+      const responseAirportsInfo = await fetch(`${apiUrl}get_ap_info?ident=${ident}`);
+      const GetAirportInfoData = await responseAirportsInfo.json();
+
+      AirportsIdents[ident] = { ...AirportsIdents[ident], ...GetAirportInfoData };
+      console.log(AirportsIdents)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  // ident[]
+  // This get each airport location to put into UI
+  // /get_ap_coordinates?ident=<ident>
+  // get AP loc by ident, return with aport lat, aport lon, both in deg
+  // return info in JSON format
+
+  try {
+
+    for (let ident in AirportsIdents) {
+      const responseGetAirportCoord = await fetch(`${apiUrl}get_ap_coordinates?ident=${ident}`);
+      const GetAirportCoord = await responseGetAirportCoord.json();
+
+      AirportsIdents[ident] = { ...AirportsIdents[ident], ...GetAirportCoord };
+      console.log(AirportsIdents);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // This get player current info from DB to UI
+  // /get_player_info?player_name=<player_name>&player_id=<player_id>
+  // get player current total traveled distance, total used time, total consumed gas, money, score
+  // this can be used to get start infos for game
+  // and during gameplay getting updated infos from back to front
+  // return info in JSON format
+  try {
+    const responseGetPlayerInfo = await fetch(`${apiUrl}get_player_info?player_name=${playerName}&player_id=${userId}`);
+    const GetPlayerInfo = await responseGetPlayerInfo.json();
+    console.log(GetPlayerInfo)
+  } catch (error) {
+    console.log(error)
+  }
+
+  // here all that shit into HTML
+  // MAP
+
+
+  // End this with waiting for user mouseclick on any available popup button
+}
+
+
+
+
+
+//
+//
+// /update_game?player_name=<player_name>&player_id=<player_id>&distance=<distance>&time_spent=<time_spent>&fuel_consumed=<fuel_consumed>
+// send player name, player id, distance traveled, time spent, fuel consumed to backend DB
+//
+//
+//
+//
+//
+// other APIs in backend
+// /distance_calculate?lati1=<lati1>&long1=<long1>&lati2=<lati2>&long2=<long2>
+// return distance in km in JSON format
+//
+// /calculeta_fuel?distance=<distance>&fuel_burn_rate=<fuel_burn_rate>
+// return used fuel in litres in JSON format
+//
+// /calculeta_time_spent?speed=<speed>&distance=<distance>
+// return time spent in hours in JSON format
+//
+//
+//
+//
+//
+//
+//
+//
+/*
+updateStatus(gameData);
+for (let airport of GetAirportsData)
+
+  if (airport.active) {
+    showWeather(airport);
+    const marker = L.market([airport.latitude_deg, airport.longitude_deg]).addTo(map);
+    const p = document.querySelector('#weather-icon')
+    marker.openPopup;
+    marker.bindPopup(p, `<b>${airport.name}</b><br>${airport.municipality}, ${airport.country}`);
+
+    marker.setIcon(greenIcon);
+
+  } else {
+    marker.setIcon(blueIcon);
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+    h4.innerHTML = `${airport.name}`;
+    p.innerHTML = `Distance ${airport.distance}km`; // how to get distance??????
+    
+
+
+
+
 
   }
-   
-  
 
-  
+*/
+
+
+function checkGame(budget) {
+  if (budget <= 0) {
+    alert(`Game Over!!!`);
+    return false;
+  }
+  return true;
 }
-// event listener to hide goal splash
+
